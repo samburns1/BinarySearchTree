3d2f3d17f44811cb9fd92e851bb13949192b1d90
Sammy Burns <sburns2023@kehillahstudent.org>
2022-09-07 00:01:08 -0700

    working ass simple linked list

--- Main.java
+++ Main.java
@@ -1,1 +1,94 @@
-//creation
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Node fe = new Node(3);
+
+        if(TestConstructor()) pass++;
+        else fail++;
+        
+        if(testSetter()) pass++;
+        else fail++;
+
+        if(testSearch()) pass++;
+        else fail++;
+
+        if(testSearchne()) pass++;
+        else fail++;
+
+        if(testSearchte()) pass++;
+        else fail++;
+        
+        if(testDelete1()) pass++;
+        else fail++;
+
+        if(testDelete2()) pass++;
+        else fail++;
+
+
+        System.out.println("passed: " + pass);
+        System.out.println("failed: " + fail);
+
+
+    }
+
+    static int pass = 0;
+    static int fail = 0;
+
+    public static boolean testDelete1(){
+        BinaryTree fl = new BinaryTree();
+        Node fe = new Node(3);
+        Node se = new Node(4);
+        fl.insert(fe);
+        fl.insert(se);
+        fl.delete(fe);
+        return fl.search(3) == null;
+    }
+
+    public static boolean testDelete2(){
+        BinaryTree fl = new BinaryTree();
+        Node fe = new Node(3);
+        Node se = new Node(4);
+        fl.insert(fe);
+        fl.insert(se);
+        fl.delete(se);
+        return fl.search(3) == fe;
+    }
+
+    public static boolean testSearchne(){
+        BinaryTree fl = new BinaryTree();
+        Node fe = new Node(3);
+        fl.insert(fe);
+        return fl.search(3) == fe;
+    }
+
+    public static boolean testSearchte(){
+        BinaryTree fl = new BinaryTree();
+        Node fe = new Node(3);
+        Node se = new Node(4);
+        fl.insert(fe);
+        fl.insert(se);
+        return fl.search(3) == fe;
+    }
+
+    public static boolean testSearch(){
+        BinaryTree fl = new BinaryTree();
+
+        return fl.search(3) == null;
+    }
+
+    public static boolean TestConstructor(){
+        Node fe = new Node(3);
+        return fe.getkey() == 3;
+
+    }
+
+    public static boolean testSetter(){
+        Node fe = new Node(3);
+        Node se = new Node(4);
+        fe.setLchild(se);
+        return fe.getLchild().getkey() == 4;
+    }
+
+
+}
--- Node.java
+++ Node.java
@@ -1,1 +1,39 @@
-//creation
+public class Node {
+    private Node parent;
+    private Node lchild;
+    private Node rchild;
+    private int key;
+    public Node(int key){
+        this.key = key;
+    }
+
+    public int getkey(){
+        return key;
+    }
+
+    public void setParent(Node parent){
+        this.parent = parent;
+    }
+
+    public void setLchild(Node lchild){
+        this.lchild = lchild;
+    }
+
+    public void setRchild(Node rchild){
+        this.rchild = rchild;
+    }
+
+    public Node getLchild(){
+        return lchild;
+    }
+
+    public Node getRchild(){
+        return rchild;
+    }
+
+    public Node getParent(){
+        return parent;
+    }
+
+
+}
