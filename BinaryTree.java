82ef8963e915654e2a916996312c0afbbe662322
Sammy Burns <sburns2023@kehillahstudent.org>
2022-09-07 00:01:08 -0700

    working as* simple list

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,1 +1,41 @@
-//creation
+public class BinaryTree {
+
+    Node root;
+
+    public Node search(int key){
+        Node x = root;
+        while( x != null && x.getkey() != key){
+            x = x.getLchild();
+        }
+        return x;
+    }
+
+    public void delete(Node a){
+        if(a.getParent() != null){
+            a.getParent().setLchild(a.getLchild());
+        }
+        else root = a.getLchild();
+
+        if(a.getLchild() != null){
+             a.getLchild().setParent(a.getParent());
+        }
+    }
+
+    public void insert(Node a){
+        a.setLchild(root);
+        if(root!=null){
+            root.setParent(a);
+        }
+        root = a;
+        a.setParent(null);
+    }
+
+
+
+
+
+
+
+
+
+}
